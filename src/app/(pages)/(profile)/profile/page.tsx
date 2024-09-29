16cc082e6f0f9f666ae8b072c42784709e85e67d
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-09-29 18:31:33 +0600

    forgot password function added

--- src/app/(pages)/(profile)/profile/page.tsx
+++ src/app/(pages)/(profile)/profile/page.tsx
@@ -1,121 +1,58 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { useResetPasswordMutation } from "@/redux/features/auth/auth.api";
+import Loader from "@/components/shared/Loader";
 import { useAppSelector } from "@/redux/hook";
-import { ErrorMessage, Field, Form, Formik } from "formik";
-import "react-phone-number-input/style.css";
-import { toast } from "sonner";
-import * as Yup from "yup";
-const initialValues = {
-  oldPassword: "",
-  password: "",
-};
-type FormValues = typeof initialValues;
-const validationSchema = Yup.object({
-  oldPassword: Yup.string().required("* Old password is required"),
-  password: Yup.string()
-    .min(6, "Password should atleast 6 character")
-    .notOneOf(
-      [Yup.ref("oldPassword"), undefined],
-      "* Your new password shouldn't match with old password"
-    )
-    .required("* New password is required"),
-});
+import { format } from "date-fns";
+import Cookies from "js-cookie";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { FaPen } from "react-icons/fa";
 
-const UpdatePassword = () => {
-  const { user, token } = useAppSelector((state) => state.auth);
+const Profile = () => {
+  const { isLoading, user } = useAppSelector((state) => state.auth);
 
-  // mutaions
-  const [resetPassword] = useResetPasswordMutation();
+  const router = useRouter();
 
-  const onSubmit = async (values: FormValues) => {
-    const toastId = toast.loading("Please wait");
-
-    try {
-      const data = await resetPassword(values);
-      const error: any = data.error;
-
-      console.log(error);
-
-      if (error) {
-        if (error.status === 403) {
-          return toast.error("password didn;t matched", {
-            description: "try to remember your password and try again",
-          });
-        }
-
-        return toast.error(error.data?.message || "Unknown error occureds");
-      }
-
-      toast.success("Passworrd updated successfully");
-    } catch (error) {
-      console.log(error);
-      toast.error("Something went wrong while updating your details");
-    } finally {
-      toast.dismiss(toastId);
-    }
-  };
+  if (isLoading) {
+    return <Loader className="!h-screen" />;
+  }
+  if (!user) {
+    Cookies.set("redirect", "/profile");
+    router.push("/");
+  }
 
   return (
-    <div className="w-full">
-      <h1 className="text-[25px] font-[600] mb-[20px]">
-        Update your login credentials
-      </h1>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={onSubmit}
-      >
-        {({ isValid }) => (
-          <Form>
-            <div className="mb-4">
-              <Label htmlFor="oldPassword">Current password *</Label>
-              <Field
-                as={Input}
-                placeholder="Type your current password"
-                type="password"
-                name="oldPassword"
-                className="mt-1 block w-full px-3 py-2 border border-borderColor rounded-md outline-none"
-              />
-              <ErrorMessage
-                name="oldPassword"
-                component="div"
-                className="text-red-500 text-sm mt-[5px]"
-              />
-            </div>
+    <div className="w-full rounded-[10px] px-[25px] py-[20px]">
+      <div className="flex items-start justify-start gap-[20px]">
+        <Link
+          href={"/profile/settings"}
+          className="w-[120px] h-[120px] rounded-full overflow-hidden bg-red-100 relative group/profile"
+        >
+          <Image
+            src={user?.image || "/images/avatar.jpg"}
+            width={120}
+            height={120}
+            alt="avatar"
+            className=" w-full h-full object-cover"
+          />
 
-            <div className="mb-4">
-              <Label htmlFor="password">New password *</Label>
-              <Field
-                type="password"
-                as={Input}
-                placeholder="Type your new password"
-                name="password"
-                className="mt-1 block w-full px-3 py-2 border border-borderColor rounded-md outline-none"
-              />
-              <ErrorMessage
-                name="password"
-                component="div"
-                className="text-red-500 text-sm mt-[5px]"
-              />
-            </div>
-
-            <Button
-              type="submit"
-              disabled={!isValid}
-              className="bg-primaryMat text-white w-[100px]"
-            >
-              Submit
-            </Button>
-          </Form>
-        )}
-      </Formik>
+          <span className="absolute top-0 left-0 bg-[#2727272f] w-full h-full scale-0 group-hover/profile:scale-[1] duration-75 rounded-full cursor-pointer center text-white">
+            <FaPen />
+          </span>
+        </Link>
+        <h3 className="text-[20px] font-[600] mt-[20px]">
+          {user?.firstName} {user?.lastName}
+        </h3>
+      </div>
+      <p className="text-primaryTxt mt-[20px]">
+        <span className="font-[600]">Email: </span> {user?.email}
+      </p>
+      <p className="text-primaryTxt mt-[8px]">
+        <span className="font-[600]">user since: </span>{" "}
+        {format(new Date(user?.createdAt || "12-30-2024"), "MMM dd, yyy")}
+      </p>
     </div>
   );
 };
 
-export default UpdatePassword;
+export default Profile;
