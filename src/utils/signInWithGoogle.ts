0417a5ed66b59b5336f9d83b69fc4107162454cc
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-11-03 21:56:27 +0600

    unused file removed

--- src/utils/signInWithGoogle.ts
+++
@@ -1,7 +0,0 @@
-import { getServerSession } from "next-auth"
-import { authOptions } from "./authOptions"
-
-export const signInWithGoogle = async () => {
- const session = await getServerSession(authOptions)
- return session
-}
